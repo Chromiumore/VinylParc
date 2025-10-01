76ba9a6bb49e24faf0c77dc53ed30045a87d8f5a
Chromiumore <parts.dima06@gmail.com>
2025-10-01 15:18:44 +0300

    application factory

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from .records import router as records_router
 
-app = FastAPI()
-app.include_router(records_router)
+def create_app():
+    app = FastAPI()
+    app.include_router(records_router)
+
+    return app
